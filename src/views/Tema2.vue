d1da64b5f135bc8f8fd4df924b01bb49b1821fd3
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-03-31 18:35:51 -0500

    Se actualizan videos.

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -18,7 +18,7 @@
 
     figure(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/qsbf2jox1sE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Gzlxr7U3gqg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .row.mt-5
       h3 Estructuras reproductivas vegetativas
